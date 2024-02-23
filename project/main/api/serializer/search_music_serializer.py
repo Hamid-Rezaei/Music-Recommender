eb6b8601cad1512fad2c25fb67a9088e45937afd
hamid <h.rezaei@asax.ir>
2024-02-23 15:55:59 +0330

    feat: search api

---
+++ project/main/api/serializer/search_music_serializer.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+
+
+class PostSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    audio = serializers.FileField(required=True, format='mp3')
+
+    def update(self, instance, validated_data):
+        pass
+
+    def create(self, validated_data):
+        pass
+
+    # def validate_image(self, value: InMemoryUploadedFile):
+    #     if value.size > 2 * 1024 * 1024:
+    #         raise serializers.ValidationError("image size should be less than 2M !")
+    #     return value
+    #
+    # def validate_content(self, value):
+    #     without_html_tag_len = CommonsUtils.calculate_text_length_without_htmltag(value)
+    #     if without_html_tag_len > 2000 or without_html_tag_len < 3:
+    #         raise serializers.ValidationError("large text!")
+    #     return value
