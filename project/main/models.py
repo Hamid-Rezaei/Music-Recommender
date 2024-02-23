f459baee061811d45d4b3eec5b3a4733ebe683bf
hamid <h.rezaei@asax.ir>
2024-02-23 21:49:19 +0330

    feat: add model

--- project/main/models.py
+++ project/main/models.py
@@ -1,3 +1,35 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class RequestStatusType(models.TextChoices):
+    PENDING = 'pending'
+    FAILURE = 'failure'
+    READY = 'ready'
+    DONE = 'done'
+
+
+class SearchRequestEntity(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    email = models.EmailField()
+    status = models.CharField(
+        choices=RequestStatusType.choices,
+        default=RequestStatusType.PENDING,
+        max_length=32
+    )
+    songID = models.UUIDField(
+        default=uuid.uuid4,
+        editable=False
+    )
+    creation_time = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        app_label = 'main'
+        db_table = 'search_request'
+
